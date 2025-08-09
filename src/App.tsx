cb979b74b9c5c86177dce637b56bfb2f5abfb28c
ali ghavidel <alighavidelma@gmail.com>
2025-08-09 12:22:40 +0330

    setup project structure and initial routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,30 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Header from "./components/Header";
+import Home from "./pages/Home";
+import Products from "./pages/Products";
+import ProductDetail from "./pages/ProductDetail";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Porfile from "./pages/Porfile";
+import Cart from "./pages/Cart";
+import Footer from "./components/Footer";
+
 export default () => {
   return (
-    <>
-      <h1 className="text-2xl text-pink-700 p-5">App components</h1>
-    </>
+    <BrowserRouter>
+      <Header />
+      <main className="min-h-[80vh] container mx-auto px-4 py-6">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/products/:id" element={<ProductDetail />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/profile" element={<Porfile />} />
+          <Route path="/cart" element={<Cart />} />
+        </Routes>
+      </main>
+      <Footer />
+    </BrowserRouter>
   );
 };
---
+++ src/pages/ProductDetail.tsx
@@ -0,0 +1,3 @@
+export default () => {
+  return <h1>product detail</h1>;
+};
