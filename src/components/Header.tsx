98ef039309e9134e85a1ace2a330932ff34763b9
ali ghavidel <alighavidelma@gmail.com>
2025-08-09 12:28:19 +0330

    header component updated

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,3 +1,18 @@
+import { Link } from "react-router-dom";
+
 export default () => {
-  return <h1>Header</h1>;
+  return (
+    <header className="bg-blue-600 text-white p-4 flex justify-between items-center">
+      <h1 className="text-xl font-bold">
+        <Link to="/">My Shop</Link>
+      </h1>
+      <nav className="flex gap-4">
+        <Link to="/">خانه</Link>
+        <Link to="/products">محصولات</Link>
+        <Link to="/cart">سبد خرید</Link>
+        <Link to="/login">ورود</Link>
+        <Link to="/register">ثبت نام</Link>
+      </nav>
+    </header>
+  );
 };
