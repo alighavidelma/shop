0b11cc61c0b99cb723f8bb8be66c54586deba58f
ali ghavidel <alighavidelma@gmail.com>
2025-08-09 12:29:11 +0330

    footer component updated

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,3 +1,7 @@
 export default () => {
-  return <h1>Footer</h1>;
+  return (
+    <footer className="bg-gray-800 text-white p-4 text-center mt-10">
+      <p>© 2025 My Shop - همه حقوق محفوظ است</p>
+    </footer>
+  );
 };
