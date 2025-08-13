f3058ad7d0294042d61842d7dca37228f811b012
ali ghavidel <alighavidelma@gmail.com>
2025-08-13 18:35:53 +0330

    login page updated with auth

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,3 +1,64 @@
+import React, { useContext, useState } from "react";
+import { AuthContext } from "../context/AuthContext";
+import { useNavigate } from "react-router-dom";
+
 export default () => {
-  return <h1>Login</h1>;
+  const { login } = useContext(AuthContext);
+  const navigate = useNavigate();
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    setError("");
+
+    if (!email || !password) {
+      setError("لظفا همه فیلدها را پر کنید.");
+      return;
+    }
+
+    try {
+      await login(email, password);
+      navigate("/profile");
+    } catch (error) {
+      setError("ایمیل یا رمزعبور اشتباه است.");
+    }
+  };
+
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <form
+        action=""
+        className="bg-white p-6 rounded-lg shadow-lg max-w-md"
+        onSubmit={handleSubmit}
+      >
+        <h2 className="text-2xl font-bold mb-4 text-center">ورود</h2>
+
+        {error && <p className="text-red-500 text-sm mb-2">{error}</p>}
+        <input
+          type="email"
+          placeholder="ایمیل"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          className="border p-2 rounded w-full mb-3"
+        />
+        <input
+          type="password"
+          placeholder="رمز عبور"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          className="border p-2 rounded w-full mb-3"
+        />
+
+        <button
+          type="submit"
+          className="bg-green-500 hover:bg-green-600 text-white py-2 rounded w-full"
+        >
+          ورود
+        </button>
+      </form>
+    </div>
+  );
 };
