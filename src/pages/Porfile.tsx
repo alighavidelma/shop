d5a31f78d59b6db5ae2862e9ed6e93821c593680
ali ghavidel <alighavidelma@gmail.com>
2025-08-13 18:45:51 +0330

    Profile updated with auth

--- src/pages/Porfile.tsx
+++ src/pages/Porfile.tsx
@@ -1,3 +1,36 @@
+import { useContext } from "react";
+import { AuthContext } from "../context/AuthContext";
+import { useNavigate } from "react-router-dom";
+
 export default () => {
-  return <h1>Profile</h1>;
+  const { user, logout } = useContext(AuthContext);
+  const navigate = useNavigate();
+
+  if (!user) {
+    return <p className="text-center mt-10">لظفا وارد شوید</p>;
+  }
+
+  return (
+    <div className="flex flex-col items-center mt-10">
+      <h1 className="text-2xl font-bold mb-4">پروفایل کاربر</h1>
+      <p>
+        <strong>نام : </strong>
+        {user.name}
+      </p>
+      <p>
+        <strong>ایمیل : </strong>
+        {user.email}
+      </p>
+
+      <button
+        className="mt-4 bg-red-500 hover:bg-red-600 text-white py-2 px-4 rounded"
+        onClick={() => {
+          logout();
+          navigate("/login");
+        }}
+      >
+        خروج
+      </button>
+    </div>
+  );
 };
